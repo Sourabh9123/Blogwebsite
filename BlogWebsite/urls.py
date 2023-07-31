47e4bfe2ac16b06fcbc601fd3bfcab29ada44e1f
sourabh <sourabhd081@gmail.com>
2023-07-31 13:25:33 +0530

    after ading static

--- BlogWebsite/urls.py
+++ BlogWebsite/urls.py
@@ -25,10 +25,10 @@ urlpatterns = [
     path('', include('App.urls')),
 ]
 
-urlpatterns = urlpatterns+static(settings.MEDIA_URL,
-document_root=settings.MEDIA_ROOT)
+# urlpatterns = urlpatterns+static(settings.MEDIA_URL,
+# document_root=settings.MEDIA_ROOT)
 
 
-# # versal 
-# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# versal 
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
