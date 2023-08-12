b3c0d51966fba4566e34f7d3e26c52982fe74053
sourabh <sourabhd081@gmail.com>
2023-08-12 15:40:08 +0530

    build test

--- build_files.sh
+++ build_files.sh
@@ -5,8 +5,8 @@ echo "Build start"
 pip install -r requirements.txt
 
 echo "Build start collectstatic"
-py manage.py collectstatic --no-input
-
+ls -la
+python manage.py collectstatic --no-input
 echo " build end new"
 
 
