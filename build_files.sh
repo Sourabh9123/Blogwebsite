0af33d24ddcc6b0d283c47640ad1c27779e81dfb
sourabh <sourabhd081@gmail.com>
2023-08-12 15:07:27 +0530

    after change py to vercel soth

--- build_files.sh
+++ build_files.sh
@@ -3,6 +3,8 @@
 echo "Build start"
 
 pip install -r requirements.txt
+echo "Build start runserver "
+python3.9 manage.py runserver
 echo "Build start collectstatic"
 python3.9 manage.py collectstatic
 
