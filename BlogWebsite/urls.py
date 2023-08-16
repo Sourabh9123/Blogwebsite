18b017589cd82b59bfe12a9f1cf8ee1288b8aef8
sourabh <sourabhd081@gmail.com>
2023-08-16 11:54:53 +0530

    after change something

--- BlogWebsite/urls.py
+++ BlogWebsite/urls.py
@@ -33,6 +33,5 @@ urlpatterns = [
 # document_root=settings.MEDIA_ROOT)
 
 
-# versal 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
